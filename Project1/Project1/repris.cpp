fbae9f2c4517b259f07fe54d77af2ab025d03aea
user <user@DESKTOP-9MJP4FH>
2025-11-20 14:03:44 +0300

    main

---
+++ Project1/Project1/repris.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	printf("Винковский\n");
+	printf("Максим\n");
+	printf("Сергеевич\n");
+	printf("\n");
+	printf("Группа: ИСЭбд-11-2025\n");
+	printf("Дата: 03.09.2025\n");
+	printf("Предмет: Основы алгоритмизации и программирования\n");
+	printf("Лабораторная работа №1\n");
+}
